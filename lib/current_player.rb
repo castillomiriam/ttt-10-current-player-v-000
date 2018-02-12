5d3192a5077e1329a8443f5195986e0bc19005e6
Miriam Castillo <castillo.m.mc@gmail.com>
2018-02-12 22:55:42 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,10 +1,10 @@
 def turn_count(board)
   counter = 0
-  board.each do {|position| puts "X" || "O" }
+  board.each do {|position| "X" || "O" }
+    count +=1
 end
 end
 
 def current_player(board)
   turn_count(board).even? ? "X" : "O"
 end
-
